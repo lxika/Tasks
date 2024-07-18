86758275e10f0da504dbf44801e79d26a221ea41
lxika <lakshi0012k@gmail.com>
2024-07-19 00:08:53 +0530

    Day 03

--- figma_task1/resources/views/index.blade.php
+++ figma_task1/resources/views/index.blade.php
@@ -58,4 +58,102 @@
     </div>
 </section>
 
+<section class="five-section text-center">
+    <div class="container third-section">
+        <div class="text-center mb-5">
+            <p class="text text-center mb-0">PostPilot makes it a <span class="cinch-text">cinch </span>to </p>
+            <p class="text text-center mb-0 mt-0">send personalized, profit- </p>
+            <p class="text text-center mt-0">generating postcards.</p>
+        </div>
+    </div>
+    <div class="container third-section">
+        <div class="row">
+            <div class="col-md-4 col-6 mb-4">
+            </div>
+            <div class="col-md-4 col-6 mb-4">
+            </div>
+            <div class="col-md-4 col-6 mb-4">
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="six-section text-center">
+    <div class="container py-5">
+        <h2 class="feature-text text-center mb-4">Everything your brand needs to <br> <span class="caption" style="color: #009387;">make your brand unforgettable</span></h2>
+        <div class="row g-4 mt-4">
+            <div class="col-md-4">
+                <div class="feature-card" style="background-color: #FDF3EA;">
+                    
+                        <img src="{{ asset('assets/image/feature-img_1.png') }}" alt="Icon" class="feature-icon">
+                    
+                    <div class="d-flex align-items-center justify-content-center">
+                        <h3 class="feature-title mt-2">Plug-and-play with your stack</h3>
+                    </div>
+                    <div class="d-flex align-items-center justify-content-center">
+                        <p class="feature-description">Native integrations provide seamless segmentation, automation & tracking. No clunky spreadsheets.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="feature-card" style="background-color: #E0F8F2;">
+                    
+                        <img src="{{ asset('assets/image/feature-img_2.png') }}" alt="Icon" class="feature-icon">
+                   
+                    <div class="d-flex align-items-center justify-content-center">
+                        <h3 class="feature-title">Run campaigns on autopilot</h3>
+                    </div>
+                    <div class="d-flex align-items-center justify-content-center">
+                        <p class="feature-description">Ink profits while you sleep. Just set it and forget it.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="feature-card " style="background-color: #EEECFF;">
+                    <img src="{{ asset('assets/image/feature-img_3.png') }}" alt="Icon" class="feature-icon heading-small text-align-center">
+                    <div class="d-flex align-items-center justify-content-center">
+                        <h3 class="feature-title">Real-time ROI dashboard</h3>
+                    </div>
+                    <div class="d-flex align-items-center justify-content-center">
+                        <p class="feature-description">Track performance of every postcard by customer or discount code. Your CFO will be happy.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="feature-card" style="background-color: #D9F2F7;">
+                    <img src="{{ asset('assets/image/feature-img_4.png') }}" alt="Icon" class="feature-icon">
+                    <div class="d-flex align-items-center justify-content-center">
+                        <h3 class="feature-title">Deliver WOW with *real* handwritten cards</h3>
+                    </div>
+                    <div class="d-flex align-items-center justify-content-center">
+                        <p class="feature-description">Our proprietary robots write and mail cards for an unforgettable WOW touch.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="feature-card" style="background-color: #FAEDED;">
+                    <img src="{{ asset('assets/image/feature-img_5.png') }}" alt="Icon" class="feature-icon">
+                    <div class="d-flex align-items-center justify-content-center">
+                        <h3 class="feature-title">Predictable pricing</h3>
+                    </div>
+                    <div class="d-flex align-items-center justify-content-center">
+                        <p class="feature-description">Unlike CPCs, postcard rates don't change based on competition or when you're targeting.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="feature-card" style="background-color: #F1F1F1;">
+                    <img src="{{ asset('assets/image/feature-img_6.png') }}" alt="Icon" class="feature-icon">
+                    <div class="d-flex align-items-center justify-content-center">
+                        <h3 class="feature-title">Complimentary concierge service</h3>
+                    </div>
+                    <div class="d-flex align-items-center justify-content-center">
+                        <p class="feature-description">DTC is in our DNA. Our experts will help your brand maximize its potential using battle-tested best practices.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
 @endsection
